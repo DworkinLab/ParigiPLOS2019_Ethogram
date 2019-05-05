bfbf60b1a20cb56f64d950eac657255eabd2da11
abhijna <abhijna.parigi@gmail.com>
2019-05-05 08:55:07 -0700

    Update Time_perState.R

--- Scripts/Time_perState.R
+++ Scripts/Time_perState.R
@@ -4,45 +4,43 @@ model_dataset <- function (h, g) {
   samplesize <- length(unique(dataset$file.num))
   dur.grooming <- function (x) {
   dur.groom <- sum(dataset$duration[dataset$state == h
-                                      & dataset$file.num == x & (dataset$pred.state == "far" | dataset$pred.state == "close") 
-                                      & dataset$predator == g])
-    dur.groom}
+  & dataset$file.num == x & (dataset$pred.state == "far" | dataset$pred.state == "close") 
+  & dataset$predator == g])
+  dur.groom}
   
   dur.groom <- sapply(1:samplesize, dur.grooming)
   
   dur.grooming.np <- function (x) {
-    dur.groom <- sum(dataset$duration[dataset$state == h 
-                                      & dataset$file.num == x & dataset$pred.state == "0" 
-                                      & dataset$predator == g])
-    dur.groom}
+  dur.groom <- sum(dataset$duration[dataset$state == h 
+  & dataset$file.num == x & dataset$pred.state == "0" 
+  & dataset$predator == g])
+  dur.groom}
   
   dur.groom.np <- sapply(1:samplesize, dur.grooming.np)
   
   
   ## Total time spent in a given predator state
   total.time <- function (x) {
-    total <- sum(dataset$duration[(dataset$pred.state == "far" | dataset$pred.state == "close") & dataset$file.num == x & dataset$predator == g])
-    total
-  }
+  total <- sum(dataset$duration[(dataset$pred.state == "far" | dataset$pred.state == "close") & dataset$file.num == x & dataset$predator == g])
+  total}
   grooming.time <- sapply(1:samplesize, total.time)
   
   total.time.np <- function (x) {
-    total <- sum(dataset$duration[dataset$pred.state == "0" & dataset$file.num == x & dataset$predator == g])
-    total
-  }
+  total <- sum(dataset$duration[dataset$pred.state == "0" & dataset$file.num == x & dataset$predator == g])
+  total}
   grooming.time.np <- sapply(1:samplesize, total.time.np) 
   
   ## Sex of select individual
   sex <- function (x) {
-    y <- dataset$sex[dataset$pred.state == "0" & dataset$file.num == x & dataset$predator == g][1]
-    y
-  }
+  y <- dataset$sex[dataset$pred.state == "0" & dataset$file.num == x & dataset$predator == g][1]
+  y}
+  
   sex.np <- sapply(1:samplesize, sex)
   
   sex.pred <- function (x) {
-    y <- dataset$sex[(dataset$pred.state == "far" | dataset$pred.state == "close") & dataset$file.num == x & dataset$predator == g][1]
-    y
-  }
+  y <- dataset$sex[(dataset$pred.state == "far" | dataset$pred.state == "close") & dataset$file.num == x & dataset$predator == g][1]
+  y}
+  
   sex.p <- sapply(1:samplesize, sex)
   
   Sex <- c(as.character(sex.np), as.character(sex.p))
@@ -64,14 +62,12 @@ model_dataset <- function (h, g) {
   
   ## Temp during recording 
   temp <- function (x) {
-    t <- dataset$temp[dataset$predator == g & dataset$file.num == x][1]
-  }
+    t <- dataset$temp[dataset$predator == g & dataset$file.num == x][1]}
   tempe <- sapply(1:samplesize, temp)
   Temp <- c(tempe, tempe)
   
   age1 <- function (x) {
-    t <- dataset$age[dataset$predator == g & dataset$file.num == x][1]
-  }
+    t <- dataset$age[dataset$predator == g & dataset$file.num == x][1]}
   age <- sapply(1:samplesize, age1)
   Age <- c(age, age)
   
@@ -79,14 +75,12 @@ model_dataset <- function (h, g) {
   
   humidity <- function (x) {
     y <- dataset$humidity[dataset$pred.state == "0" & dataset$file.num == x & dataset$predator == g][1]
-    y
-  }
+    y}
   humidity.np <- sapply(1:samplesize, humidity)
   
   humidity.pred <- function (x) {
     y <- dataset$humidity[(dataset$pred.state == "far" | dataset$pred.state == "close") & dataset$file.num == x & dataset$predator == g][1]
-    y
-  }
+    y}
   humidity.p <- sapply(1:samplesize, humidity.pred)
   
   Humidity <- c(as.character(humidity.np), as.character(humidity.p))
@@ -105,7 +99,6 @@ model_dataset <- function (h, g) {
   
   ## Making a dataframe
   spiderdata.grooming <- data.frame(file.num, Sex, Age, Date, rec.time, Humidity, Temp, behav, total.dur, time, pred.type, pred.state)
-  return (spiderdata.grooming)
-}
+  return (spiderdata.grooming)}
 
 ############# END OF FUNCTION #################
